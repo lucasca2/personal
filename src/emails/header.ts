5d9d2b3dc909ab7ac5ac3c56026656d2eb431077
Lucas Amaral <lucas.costamaral@gmail.com>
2024-12-11 14:55:22 -0300

    fix: Fixed email image and flags spaces

--- src/emails/header.ts
+++ src/emails/header.ts
@@ -1,7 +1,7 @@
 export const renderHeader = () => `
     <mj-section padding="32px 0">
         <mj-column padding="0">
-        <mj-image src="https://lucas.amaral.dev.br/lucas.jpeg" align="left" height="40px" width="40px" padding="0" border-radius="40px"></mj-image>
+        <mj-image src="https://lucas.amaral.dev.br/lucas-email.png" align="left" height="40px" width="40px" padding="0" border-radius="40px"></mj-image>
         </mj-column>
     </mj-section>
 `;
